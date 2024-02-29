8d8e76bee145c9a2929a9b43a464ee9dc3b66931
ayoubleass <ayoubleas@gmail.com>
2024-02-29 05:53:45 -0800

    File: 16-binary_tree_is_perfect.c, 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-		return (NULL)
+		return (NULL);
 	if (node->parent->right == node && node->parent->left == NULL)
 		return (NULL);
 	if (node->parent->left == node && node->parent->right == NULL)
